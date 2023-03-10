4aba1ca444670055b8a9f60b5ca7146b75b0ccec
Dobrocsi Kornél (SZF_2022_2024) <dobrocsi.kornl@diak.szamalk-szalezi.hu>
2023-03-10 13:26:30 +0100

    alt

---
+++ main.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", init);
+
+function init() {
+  const articleELEM = document.querySelectorAll("article");
+  const button = document.querySelector(".bal");
+  const button2 = document.querySelector(".jobb");
+  const tarto = [
+    "kepek/DSC7025.jpg",
+    "kepek/DSC7365.jpg",
+    "kepek/DSC7371_1.jpg",
+    "kepek/DSC7444.jpg",
+    "kepek/DSC7515.jpg",
+  ];
+  const tort = document.querySelector("img");
+  elhelyezes(tarto, articleELEM);
+  const Kiskepek = document.querySelectorAll("article  img");
+  for (let index = 0; index < tarto.length; index++) {
+    Kiskepek[index].addEventListener("click", function(){
+      fuggveny(index,tarto)
+  })
+}
+}
+
+function elhelyezes(tarto, articleELEM) {
+  articleELEM[0].innerHTML += "<div>";
+  for (let i = 0; i < tarto.length; i++) {
+    articleELEM[0].innerHTML += `<img src=${tarto[i]} alt="kep">`;
+  }
+  articleELEM[0].innerHTML += "</div>";
+}
+function fuggveny(index, tarto) {
+  const nagykep = document.querySelectorAll("section div img");
+  console.log(nagykep);
+  nagykep[0].src = tarto[index];
+}
