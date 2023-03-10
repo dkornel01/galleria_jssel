95ebeb81661117fb281838fbb7fa8b96aead3ec4
Dobrocsi Kornél (SZF_2022_2024) <dobrocsi.kornl@diak.szamalk-szalezi.hu>
2023-03-10 13:54:17 +0100

    nagy

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 window.addEventListener("load", init);
 
 function init() {
-  const articleELEM = document.querySelectorAll("article");
-  const button = document.querySelector(".bal");
-  const button2 = document.querySelector(".jobb");
-  const tarto = [
+  const ARTICLELEM = document.querySelectorAll("article");
+  const BUTTON = document.querySelector(".bal");
+  const BUTTON2 = document.querySelector(".jobb");
+  const TARTO = [
     "kepek/DSC7025.jpg",
     "kepek/DSC7365.jpg",
     "kepek/DSC7371_1.jpg",
     "kepek/DSC7444.jpg",
     "kepek/DSC7515.jpg",
   ];
-  const tort = document.querySelector("img");
-  elhelyezes(tarto, articleELEM);
-  const Kiskepek = document.querySelectorAll("article  img");
-  for (let index = 0; index < tarto.length; index++) {
-    Kiskepek[index].addEventListener("click", function () {
-      fuggveny(index, tarto);
+  const TORT = document.querySelector("img");
+  elhelyezes(TARTO, ARTICLELEM);
+  const KISKEPEK = document.querySelectorAll("article  img");
+  for (let index = 0; index < TARTO.length; index++) {
+    KISKEPEK[index].addEventListener("click", function () {
+      fuggveny(index, TARTO);
     });
   }
 }
 
-function elhelyezes(tarto, articleELEM) {
-  for (let i = 0; i < tarto.length; i++) {
-    articleELEM[0].innerHTML += `<img src=${tarto[i]} alt="kep" >`;
+function elhelyezes(TARTO, ARTICLELEM) {
+  for (let i = 0; i < TARTO.length; i++) {
+    ARTICLELEM[0].innerHTML += `<img src=${TARTO[i]} alt="kep" >`;
   }
-  articleELEM[0].innerHTML += "<div>";
-  
+  ARTICLELEM[0].innerHTML += "<div>";
+
 }
-function fuggveny(index, tarto) {
-  const nagykep = document.querySelectorAll("section div img");
-  console.log(nagykep);
-  nagykep[0].src = tarto[index];
+function fuggveny(index, TARTO) {
+  const NAGYKEP = document.querySelectorAll("section div img");
+  console.log(NAGYKEP);
+  NAGYKEP[0].src = TARTO[index];
 }
